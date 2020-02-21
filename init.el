8895171f9d277181ff319c43ed6aea29fb01566d
Chris Mazzullo <chris.mazzullo@gmail.com>
2020-02-21 09:59:58 -0500

    Fixed merge issue

--- init.el
+++ init.el
@@ -163,16 +163,11 @@
 (setq web-mode-engines-alist '(("django" . "\\.html?\\'")))
 
 
-;; EMMET ;;
-(add-hook 'sgml-mode-hook 'emmet-mode)
-(add-hook 'web-mode-hook 'emmet-mode)
-(add-hook 'css-mode-hook 'emmet-mode)
-
-
 ;; HOOKS
 
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
 
 ; Clean trailing whitespace before saving:
 (add-to-list 'write-file-functions 'delete-trailing-whitespace)
