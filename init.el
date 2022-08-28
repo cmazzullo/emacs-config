11efaa5d5e1e23e5057053b6f842fec4b5895fa2
Chris Mazzullo <chris.mazzullo@gmail.com>
2022-08-27 21:47:13 -0400

    org-startup-truncated should be t

--- init.el
+++ init.el
@@ -70,6 +70,7 @@
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode)) ; Always treat .m as a matlab extension
 
 ;; shortcuts for git-grepping file types:
+(require 'grep)
 (mapc (lambda (x) (add-to-list 'grep-files-aliases x))
       '(("py" . "*.py")
 	("html" . "*.html")
@@ -200,4 +201,4 @@
       org-src-window-setup 'current-window  ;; edit source blocks in the same window
       org-src-preserve-indentation t  ;; prevents source blocks from indenting their content
       org-file-apps '((auto-mode . emacs)) ;; Always open org links in emacs (not firefox)
-      org-startup-truncated nil)
+      org-startup-truncated t) ;; truncate lines so tables look better
